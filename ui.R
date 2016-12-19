71bda21e6738ed54a1d82b355ca002620966a4b8
davesteps <davesteps@gmail.com>
2016-12-19 21:10:04 +0000

    iframe

--- ui.R
+++ ui.R
@@ -5,6 +5,7 @@ shinyUI(  dashboardPage(
   dashboardSidebar(disable = T),
   dashboardBody(
     box(width = 6,leafletOutput('map1')),
+    box(width = 6,tags$iframe(src = "http://localhost:8081",width=400,height=300, seamless=NA)),
     box(verbatimTextOutput('test'))
     
   )
