8c3d15d69db963a519a14c67966bfd70a06efbf3
davesteps <davesteps@gmail.com>
2016-12-19 14:11:14 +0000

    loggin working

--- gpslog.sh
+++ gpslog.sh
@@ -2,6 +2,7 @@
 
 while true
 do
-	gpspipe -w -n 10 |   grep -m 1 lon | cat > /home/pi/Cydorg/gps.log
+	gpspipe -w -n 10 |   grep -m 1 lon > /home/pi/Cydorg/gps.log
+	#gpspipe -w -n 10  > /home/pi/Cydorg/gps.log
 	sleep 5
 done
