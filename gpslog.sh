4669be53fb82efa9f6a5e67a9a6d8b9a11e7ec79
davesteps <davesteps@gmail.com>
2016-12-19 11:44:15 +0000

    gpsdata

--- gpslog.sh
+++ gpslog.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 gpsdata=$( gpspipe -w -n 10 |   grep -m 1 lon )
-lat=$( echo "$gpsdata"  | jsawk 'return this.lat' )
-lon=$( echo "$gpsdata"  | jsawk 'return this.lon' )
-echo "You are here: $lat, $lon - https://maps.google.com/maps?q=$lat,+$lon"
+echo "$gpsdata"
+
