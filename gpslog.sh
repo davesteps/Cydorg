07780c59b2028ac2156b8051e78224af21e271a9
davesteps <davesteps@gmail.com>
2016-12-19 13:29:23 +0000

    sep loggers

--- gpslog.sh
+++ gpslog.sh
@@ -2,7 +2,6 @@
 
 while true
 do
-	#gpspipe -w -n 10 |   grep -m 1 lon > /home/pi/Cydorg/gps.log
-	temper > /home/pi/Cydorg/temp.log
+	gpspipe -w -n 10 |   grep -m 1 lon | cat > /home/pi/Cydorg/gps.log
 	sleep 5
 done
---
+++ templog.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true
+do
+	temper > /home/pi/Cydorg/temp.log
+	sleep 5
+done
