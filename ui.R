f661e396ab73f64faae4b23c51c5c6160e9eb96a
davesteps <davesteps@gmail.com>
2016-12-21 18:33:24 +0000

    spd

--- ui.R
+++ ui.R
@@ -7,7 +7,8 @@ shinyUI(  dashboardPage(
     box(width = 4,leafletOutput('map1',height = 300)),
     box(width = 4,tags$iframe(src = "http://192.168.1.78:8081",width=324,height=244, seamless='seamless')),
     # box(width = 4,plotOutput('tempPlot')),
-    valueBoxOutput('tempVal',width = 2)
+    valueBoxOutput('tempVal',width = 2),
+    valueBoxOutput('speedVal',width = 2)
     
   )
 )
