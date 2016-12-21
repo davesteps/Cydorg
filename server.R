75623a495e31508a90bcb601b39d7bc4bff8e558
davesteps <davesteps@gmail.com>
2016-12-21 18:32:26 +0000

    gps

--- server.R
+++ server.R
@@ -1,7 +1,7 @@
 
 
 
-
+# pi : pw 99*****s!
 
 
 
@@ -14,8 +14,9 @@ parseTemp <- function(f){
 
 parseGPS <- function(f){
   fl <- lapply(readLines(f),jsonlite::fromJSON)
-  plyr::ldply(fl,as.data.frame)
+  plyr::ldply(fl,as.data.frame,stringsAsFactors = F)
 }
+
 
 shinyServer(function(input, output, session) {
   
@@ -27,19 +28,28 @@ shinyServer(function(input, output, session) {
   })
   output$tempPlot <- renderPlot({
     invalidateLater(60e3)
-    ggplot(tempLog,aes(x=V1,y=V2))+geom_point()
+    if(nrow(tempLog)<10) return(NULL)
+    ggplot(tempLog,aes(x=V1,y=V2))+geom_boxplot()
   })
   output$tempVal <- renderValueBox({
-    valueBox(tempCrnt()$V2,'C',icon = icon('thermometer'),width = 2)
+    valueBox(tempCrnt()$V2,subtitle = 'C',icon = icon('thermometer-0'),width = 2)
   })
   
-  # gpsLog <- parseGPS('gps.log')
-  # makeReactiveBinding('gpsLog')
-  # gpsCrnt <- reactiveFileReader(2000,session,filePath = 'gps.current',readFunc = parseGPS)
+  gpsLog <- parseGPS('gps.log')
+  gpsCrnt <- reactiveFileReader(2000,session,filePath = 'gps.current',readFunc = parseGPS)
   
+  observeEvent(gpsCrnt(),{
+    gpsLog <<- rbind(gpsLog,gpsCrnt())
+  })
+  output$speedVal <- renderValueBox({
+    valueBox(gpsCrnt()$speed,subtitle = 'm/s',width = 2)
+  })
   
-  
-  
+  # output$altPlot <- renderPlot({
+  #   invalidateLater(60e3)
+  #   if(nrow(gpsLog)<10) return(NULL)
+  #   ggplot(gpsLog,aes(x=V1,y=V2))+geom_boxplot()
+  # })
   
   # gps <- reactiveFileReader(2000,session,filePath = 'gps.log',readFunc = readGPS)
   # 
--- ui.R
+++ ui.R
@@ -4,10 +4,10 @@ shinyUI(  dashboardPage(
   dashboardHeader(title = 'Cydorg'),
   dashboardSidebar(disable = T),
   dashboardBody(
-    box(width = 4,leafletOutput('map1')),
+    box(width = 4,leafletOutput('map1',height = 300)),
     box(width = 4,tags$iframe(src = "http://192.168.1.78:8081",width=324,height=244, seamless='seamless')),
-    box(width = 4,plotOutput('tempPlot')),
-    valueBoxOutput('tempVal')
+    # box(width = 4,plotOutput('tempPlot')),
+    valueBoxOutput('tempVal',width = 2)
     
   )
 )
