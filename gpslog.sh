f5acd34217745242e682e8c6b1a61f2ed11b8c62
davesteps <davesteps@gmail.com>
2016-12-21 19:30:32 +0000

    dgdg

--- gpslog.sh
+++ gpslog.sh
@@ -4,8 +4,13 @@ rm /home/pi/Cydorg/gps.log
 
 while true
 do
-	g=$(gpspipe -w -n 10 |   grep -m 1 lon)
-	echo $g >> /home/pi/Cydorg/gps.log
-	echo $g > /home/pi/Cydorg/gps.current
+  gpsdata=$( gpspipe -w -n 10 | grep -m 1 lon )
+  time=$(echo "$gpsdata" | jq -r '.time')
+  lat=$(echo "$gpsdata" | jq -r '.lat')
+  lon=$(echo "$gpsdata" | jq -r '.lon')
+  alt=$(echo "$gpsdata" | jq -r '.alt')
+  speed=$(echo "$gpsdata" | jq -r '.speed')
+  echo "$time,$lat,$lon,$alt,$speed" >> /home/pi/Cydorg/gps.log
+  echo "$time,$lat,$lon,$alt,$speed" > /home/pi/Cydorg/gps.current
 	sleep 3
 done
